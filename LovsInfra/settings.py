44c08c48fa61f8c77a7d8414a3ac0b15714b8318
Srichakradhar Reddy Nagireddy <srichakra3nsr3@gmail.com>
2017-08-05 04:53:54 +0530

    add lovs-infra.herokuapp.com to ALLOWED_HOSTS

--- LovsInfra/settings.py
+++ LovsInfra/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'foh!06bw47i(cqqgvz)(4&g-&9ba*m$z-w#5910u4x+nf7et9d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lovs-infra.herokuapp.com']
 
 # Application definition
 
@@ -75,6 +75,14 @@ WSGI_APPLICATION = 'LovsInfra.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'lovsinfra',
+    #     'USER': 'chuck',
+    #     'PASSWORD': '',
+    #     'HOST': 'localhost',  # Or an IP Address that your DB is hosted on
+    #     'PORT': '5432',
+    # },
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'd8sdckvc6gicpi',
