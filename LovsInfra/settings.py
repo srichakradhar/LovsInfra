e2764bc869d5fe5e1cd07c84a39ab65ff9b023b9
Srichakradhar Reddy Nagireddy <srichakra3nsr3@gmail.com>
2017-08-05 04:36:57 +0530

    add: database settings for heroku

--- LovsInfra/settings.py
+++ LovsInfra/settings.py
@@ -142,3 +142,11 @@ MEDIA_URL = '/static/media/'
 STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'),)
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
